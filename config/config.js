9ac26f1d1185c4e0f8d5d85e3bb7abe9837db20c
Alejandro F <afuste7@gmail.com>
2020-04-08 20:44:36 -0400

    changed eslint errors

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-	development: {
-		username: process.env.DB_USER,
-		password: process.env.PASS,
-		database: process.env.DATABASE,
-		host: process.env.HOST,
-		dialect: 'mysql',
-		operatorsAliases: false
-	},
-	test: {
-		username: process.env.DB_USER,
-		password: process.env.PASS,
-		database: process.env.DATABASE,
-		host: process.env.HOST,
-		dialect: 'mysql',
-		operatorsAliases: false
-	},
-	production: {
-		// may change later
-		use_env_variable: 'blah',
-		dialect: 'mysql'
-	}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  production: {
+    // may change later
+    useEnvVariable: "blah",
+    dialect: "mysql"
+  }
 };
