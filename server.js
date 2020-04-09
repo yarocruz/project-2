fa8d52158fff81d40bff53a52e2bf97d19873c7a
Alejandro F <afuste7@gmail.com>
2020-04-08 20:27:07 -0400

    added .env file to root folder, added variables to .env file, added config.js file to config folder, and added code to index js file

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+require('dotenv').config();
+
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+const passport = require('./config/passport');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+	app.listen(PORT, function() {
+		console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+	});
 });
