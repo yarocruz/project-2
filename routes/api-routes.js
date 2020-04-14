c5b2ebd21c5d7ce6bab9b5b8f9baa343f4480609
yarocruz <yarocruz@gmail.com>
2020-04-14 11:50:54 -0400

    added an api/collections route to test out seeded data. collections page is no setup to render based on what's on the collections table

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,13 +3,11 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = app => {
-  // app.post("/api/collections", (req, res) => {
-  //   db.Collection.create({
-
-  //     podcastTitle: req.body.title,
-  //     collectionTitle: req.body.collectionTitle
-  //   });
-  // });
+  app.get("/api/collections", (req, res) => {
+    db.Collection.findAll({}).then(collection => {
+      res.json(collection);
+    });
+  });
 
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
