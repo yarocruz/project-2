e1ce4c4184b60ad6b561ef21514d9ce9d46197ca
Alejandro F <afuste7@gmail.com>
2020-04-15 19:09:49 -0400

    fixed eslint error

--- routes/review-api-routes.js
+++ routes/review-api-routes.js
@@ -6,44 +6,40 @@
 // =============================================================
 
 // Requiring our models
-var db = require('../models');
+var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = (app) => {
-	// GET route for getting all of the reviews
-	app.get('/api/reviews', (req, res) => {
-		db.Review.findAll({}).then((dbReview) => res.json(dbReview));
-	});
+module.exports = app => {
+  // GET route for getting all of the reviews
+  app.get("/api/reviews", (req, res) => {
+    db.Review.findAll({}).then(dbReview => res.json(dbReview));
+  });
 
-	// Get route for retrieving a single review
-	app.get('/api/reviews/:id', (req, res) => {
-		// Here we add an "include" property to our options in our findOne query
-		db.Review
-			.findOne({
-				where: {
-					id: req.params.id
-				}
-			})
-			.then((dbReview) => res.json(dbReview));
-	});
+  // Get route for retrieving a single review
+  app.get("/api/reviews/:id", (req, res) => {
+    // Here we add an "include" property to our options in our findOne query
+    db.Review.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbReview => res.json(dbReview));
+  });
 
-	// POST route for saving a new user
-	app.post('/api/reviews', (req, res) => {
-		// Using parseInt to change user input from string to integer
-		const payload = { ...req.body, rating: parseInt(req.body.rating) };
+  // POST route for saving a new user
+  app.post("/api/reviews", (req, res) => {
+    // Using parseInt to change user input from string to integer
+    const payload = { ...req.body, rating: parseInt(req.body.rating) };
 
-		db.Review.create(payload).then((dbReview) => res.json(dbReview));
-	});
+    db.Review.create(payload).then(dbReview => res.json(dbReview));
+  });
 
-	// DELETE route for deleting reviews
-	app.delete('/api/reviews/:id', (req, res) => {
-		db.Review
-			.destroy({
-				where: {
-					id: req.params.id
-				}
-			})
-			.then((dbReview) => res.json(dbReview));
-	});
+  // DELETE route for deleting reviews
+  app.delete("/api/reviews/:id", (req, res) => {
+    db.Review.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbReview => res.json(dbReview));
+  });
 };
